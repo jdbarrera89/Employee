b739d488ca3e2b788391ae0db9b79e27e89bb283
JOHN DAVID BARRERA ZAPATA <johnbarrera214888@correo.itm.edu.co>
2021-09-05 01:23:42 -0500

    Add entity

---
+++ Employee.Common/Models/EmployeeTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee.Common.Models
+{
+    public class EmployeeTime
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ Employee.Functions/Entities/EmployeeTimeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Functions.Entities
+{
+    public class EmployeeTimeEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
